6e2d3bdd9e2f97484421627fccc3e17ad250cbeb
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-02-05 14:25:15 +0100

    finalizo el ejercicio 1 parte a

---
+++ ej1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Introduzca su nombre: " nombre
+read -p "Introduzca su primer apellido: " apellido1
+read -p "Introduzca su segundo apellido: " apellido2
+
+sleep 5
+
+echo "Su nombre completo es $nombre $apellido1 $apellido2"
